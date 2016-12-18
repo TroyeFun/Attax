2657ea338e05d4718324fc09bb069d335b6e1db9
TroyeFun <953690224@qq.com>
2016-12-19 01:04:58 +0800

    Create player.hpp

---
+++ player.hpp
@@ -0,0 +1,14 @@
+#ifndef player_hpp
+#define player_hpp
+
+#include <stdio.h>
+void delay();
+void mode();
+extern int MODE;
+void player1();
+void player2();
+bool end(int player);
+extern int sum1,sum2;
+
+#endif /* player_hpp */
+
